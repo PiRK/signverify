9afb0367c8c70328cd8d3e3dbcbbdd4b2e217e53
PiRK <pierrekn@gmail.com>
2020-12-30 09:47:27 +0100

    add and entry point for the application
    On `pip install .`, a `signverify` script will we installed on the PATH

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-
 from setuptools import setup, find_packages
 
 setup(
-    name='signverify',
-    version='1.0.0',
-    url='https://github.com/PiRK/signverify',
-    author='PiRK',
-    description='Desktop GUI application to sign and verify a message using'
-                ' bitcoin private/public keys',
+    name="signverify",
+    version="1.0.0",
+    url="https://github.com/PiRK/signverify",
+    author="PiRK",
+    description="Desktop GUI application to sign and verify a message using"
+    " bitcoin private/public keys",
     packages=find_packages(),
-    install_requires=['PySide2', 'ecdsa', 'ElectrumABC'],
+    install_requires=["PySide2", "ecdsa", "ElectrumABC"],
+    entry_points={"gui_scripts": ["signverify = signverify.__main__:main"]},
 )
--- signverify/__main__.py
+++ signverify/__main__.py
@@ -2,7 +2,15 @@
 from PySide2 import QtWidgets
 from .gui import MainWindow
 
-app = QtWidgets.QApplication([])
-mw = MainWindow()
-mw.show()
-app.exec_()
+
+def main():
+    """Application's entry point defined in setup.py"""
+    app = QtWidgets.QApplication([])
+    mw = MainWindow()
+    mw.show()
+    app.exec_()
+
+
+# This is reached if the package is executed with `python -m signverify`
+if __name__ == '__main__':
+    main()
